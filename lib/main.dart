d59b69da62aecb57ee9a686b07d04b9acad36f4b
Mehul Minat <mehulminat321@gmail.com>
2022-05-24 23:25:34 +0530

    test1

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo  Page'),
+      home: const MyHomePage(title: 'Flutter  Page'),
     );
   }
 }
